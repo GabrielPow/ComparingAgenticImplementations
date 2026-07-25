2885f4e1ab127484407b45775c5378491c02e655
Gabriel Pow <105570743+GabrielPow@users.noreply.github.com>
2026-07-25 16:21:49 -0300

    Evaluator Final Bug Fix?

--- evaluator.py
+++ evaluator.py
@@ -61,7 +61,8 @@ def evaluate_with_judge(item: dict, system_output_text: str) -> dict:
 
     try:
         response = client.models.generate_content(
-            formatted_prompt,
+            model=model_id,
+            prompt=formatted_prompt,
             config=types.GenerateContentConfig(response_mime_type="application/json")
         )
         return json.loads(response.text)
